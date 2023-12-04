a1f1b6091782060422b0a4c36cbe3da89881200e
amalkanil1999 <amalkanilkolakot@gmail.com>
2023-12-04 18:07:44 +0530

    authentication_frontend_compeleted

--- api/v1/auth/serializers.py
+++ api/v1/auth/serializers.py
@@ -1,22 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model, authenticate
-
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['id','name','password','email']
-#         extra_kwargs = {
-#             'password':{'write_only':True},
-#         }
-
-#     def create(self,validated_data):
-#         password = validated_data.pop('password', None)
-#         instance=self.Meta.model(**validated_data)
-#         if password is not None:
-#             instance.set_password(password)
-#         instance.save()
-#         return instance
+from rest_framework.exceptions import ValidationError
 
 UserModel = get_user_model()
 
@@ -24,26 +8,26 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = '__all__'
-    def create(self, clean_data):
-        user_obj = UserModel.objects.create_user(email=clean_data['email'],
-            password = clean_data['password'])
-        user_obj.username = clean_data['username']
-        user_obj.save()
+
+    def create(self, validated_data):
+        user_obj = UserModel.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            username=validated_data['username']
+        )
         return user_obj
-    
 
 class UserLoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
-    
-    def check_user(self,clean_data):
-        user=authenticate(username=clean_data['email'], password=clean_data['password'])
-        if not user:
-            raise serializers.ValidationError('user not found')
-        return user
-    
+	email = serializers.EmailField()
+	password = serializers.CharField()
+	##
+	def check_user(self, clean_data):
+		user = authenticate(username=clean_data['email'], password=clean_data['password'])
+		if not user:
+			raise ValidationError('user not found')
+		return user
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model: UserModel
-        fields = ('email', 'username')
+	class Meta:
+		model = UserModel
+		fields = ('email', 'username')
--- users/models.py
+++ users/models.py
@@ -1,39 +1,32 @@
-
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
-
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class AppUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, username, password=None, **extra_fields):
         if not email:
-            raise ValueError('Email is required')
+            raise ValueError('An email is required.')
         if not password:
-            raise ValueError('Password is required')
+            raise ValueError('A password is required.')
         email = self.normalize_email(email)
-        user = self.model(email=email)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
-    def create_superuser(self, email, password=None):
-        if not email:
-            raise ValueError('Email is required.')
-        if not password:
-            raise ValueError('Password is required')
-        user = self.create_user(email, password)
-        user.is_superuser = True
-        user.save()
-        return user
-    
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, username, password, **extra_fields)
+
+
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
-    user_id = models.AutoField(primary_key=True)
-    username= models.CharField(max_length=255)
-    email = models.EmailField(max_length=255, unique=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    objects = AppUserManager()
-    def __str__(self):
-        return self.username
+	user_id = models.AutoField(primary_key=True)
+	email = models.EmailField(max_length=50, unique=True)
+	username = models.CharField(max_length=50)
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+	objects = AppUserManager()
+	def __str__(self):
+		return self.username
